deea36890f2675d811041f7e1649f24f8b5081bb
Vladyslav Chyzhevskyi <vladyslav@chyzhevskyi.com>
2020-09-11 22:03:57 +0200

    added IQuery without parameters and output

--- Queries/IQuery.cs
+++ Queries/IQuery.cs
@@ -1,5 +1,10 @@
 namespace VladyslavChyzhevskyi.ASPNET.CQRS.Queries
 {
+    public interface IQuery
+    {
+        void Execute();
+    }
+
     public interface IQuery<TQueryParameters, TQueryResult>
     {
         TQueryResult Execute(TQueryParameters parameters);
